35c0a379845d47a0c30cb2baec0b134f03dd72a2
Matthew Palmer <matthewandrewpalmer@outlook.com>
2020-12-16 11:57:34 +0000

    LU-8001 Change error message and add test

--- src/Components/ONSDesignSystem/ONSErrorPanel.tsx
+++ src/Components/ONSDesignSystem/ONSErrorPanel.tsx
@@ -6,13 +6,7 @@ function ONSErrorPanel(): ReactElement {
             <div className="panel panel--error panel--simple">
                 <div className="panel__body">
                     <p>
-                        Telephone Operations Blaise Interface is unable to verify or display current active questionnaires.
-                        You may try resolving this by <strong>reloading</strong> this page to refresh table content.
-                    </p>
-                    <br/>
-                    <p>
-                        If this does not help and you are still experiencing problems <a
-                        href="https://ons.service-now.com/">report this issue</a> to Service Desk.
+                        Sorry, there is a problem with this service. We are working to fix the problem. Please try again later.
                     </p>
                 </div>
             </div>
