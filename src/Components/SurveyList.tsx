1ae0564eae804261a8f68c6c88b75e94dd354a4d
Matthew Palmer <matthewandrewpalmer@outlook.com>
2020-12-22 12:00:52 +0000

    Change UI to show panel before table loads in

--- src/Components/SurveyList.tsx
+++ src/Components/SurveyList.tsx
@@ -16,43 +16,45 @@ function SurveyList(props: Props): ReactElement {
     const {list, listError} = props;
 
     return <>
-        <h3>Surveys</h3>
-        <table id="survey-table" className="table ">
-            <thead className="table__head u-mt-m">
-            <tr className="table__row">
-                <th scope="col" className="table__header ">
-                    <span>Survey three letter acronym</span>
-                </th>
-                <th scope="col" className="table__header ">
-                    <span>Link to questionnaires</span>
-                </th>
-            </tr>
-            </thead>
-            <tbody className="table__body">
-            {
-                list && list.length > 0
-                    ?
-                    list.map((item: Survey) => {
-                        return (
-                            <tr className="table__row" key={item.survey} data-testid={"survey-table-row"}>
-                                <td className="table__cell ">
-                                    {item.survey}
-                                </td>
-                                <td className="table__cell ">
-                                    <Link to={"/survey/" + item.survey}>View active questionnaires</Link>
-                                </td>
-                            </tr>
-                        );
-                    })
-                    :
-                    <tr>
-                        <td className="table__cell " colSpan={3}>
-                            {listError.message}
-                        </td>
+        <h2>Surveys</h2>
+        {
+            list && list.length > 0
+                ?
+                <table id="survey-table" className="table ">
+                    <thead className="table__head u-mt-m">
+                    <tr className="table__row">
+                        <th scope="col" className="table__header ">
+                            <span>Survey three letter acronym</span>
+                        </th>
+                        <th scope="col" className="table__header ">
+                            <span>Link to questionnaires</span>
+                        </th>
                     </tr>
-            }
-            </tbody>
-        </table>
+                    </thead>
+                    <tbody className="table__body">
+                    {
+                        list.map((item: Survey) => {
+                            return (
+                                <tr className="table__row" key={item.survey} data-testid={"survey-table-row"}>
+                                    <td className="table__cell ">
+                                        {item.survey}
+                                    </td>
+                                    <td className="table__cell ">
+                                        <Link to={"/survey/" + item.survey}>View active questionnaires</Link>
+                                    </td>
+                                </tr>
+                            );
+                        })
+                    }
+                    </tbody>
+                </table>
+                :
+                <div className="panel panel--info panel--simple u-mb-m">
+                    <div className="panel__body">
+                        <p>{listError.message}</p>
+                    </div>
+                </div>
+        }
     </>;
 }
 
