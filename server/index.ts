bd23adc551e8b1389d220d3cd822dd7f9ab4f761
Matthew Palmer <matthewandrewpalmer@outlook.com>
2021-03-23 10:30:24 +0000

    Add profiler

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,9 @@
 import app from "./server";
+import * as profiler from "@google-cloud/profiler";
+
+profiler.start({logLevel: 4}).catch((err: unknown) => {
+    console.log(`Failed to start profiler: ${err}`);
+});
 
 const port: string = process.env.PORT || "5000";
 app.listen(port);
