5cddb7cc2262761fa4f9674f86409a50ab1260fc
Alistair Grew <Ali.Grew@cloudsolutions.co.uk>
2021-07-22 15:39:32 +0100

    Tobi health Check Endpoint (#380)
    * Change health endpoint URL
    
    * Fix health endpoint test
    
    * Try simplifying
    
    * Try this endpoint
    
    * Matthew suggested health check
    
    * Hopefully this fixes it

--- server/tests/index.test.js
+++ server/tests/index.test.js
@@ -6,10 +6,10 @@ const request = supertest(app);
 
 describe("Test Heath Endpoint", () => {
     it("should return a 200 status and json message", async done => {
-        const response = await request.get("/health_check");
+        const response = await request.get("/tobi-ui/version/health");
 
         expect(response.statusCode).toEqual(200);
-        expect(response.body).toStrictEqual({status: 200});
+        expect(response.body).toStrictEqual({healthy: true});
         done();
     });
 });
