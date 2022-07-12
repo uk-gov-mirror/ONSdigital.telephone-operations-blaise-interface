be33820205c69156fc2685b6d364fc2e2b0a2336
Tom Oram <tom.oram@armakuni.com>
2022-07-12 11:14:55 +0100

    fix: Extend timeout
    We're currently getting errors that the timeout has been exceeded.
    Hopefully this will quieten the errors, but it will probably be
    worth looking into the upstream cause in the future.
    
    Refs: BLAIS5-3207

--- server/server.ts
+++ server/server.ts
@@ -8,7 +8,7 @@ import {getEnvironmentVariables} from "./Config";
 
 const server = express();
 
-axios.defaults.timeout = 10000;
+axios.defaults.timeout = 15000;
 
 if (process.env.NODE_ENV !== "production") {
     dotenv.config();
