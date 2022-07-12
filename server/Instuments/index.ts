9e5c9f9130f7a32db4c33bcdbac8e55a9b402498
Tom Oram <tom.oram@armakuni.com>
2022-07-12 13:15:16 +0100

    refactor: Extract named functions
    Ref: BLAIS5-3207

--- server/Instuments/index.ts
+++ server/Instuments/index.ts
@@ -6,6 +6,13 @@ import { fieldPeriodToText } from "../Functions";
 import AuthProvider from "../AuthProvider";
 import { Logger } from "../Logger";
 
+function groupBySurvey(activeInstruments: Instrument[]) {
+    return _.chain(activeInstruments)
+        .groupBy("surveyTLA")
+        .map((value: Instrument[], key: string) => ({ survey: key, instruments: value }))
+        .value();
+}
+
 export default function InstrumentRouter(
     blaiseApiUrl: string,
     vmExternalWebUrl: string,
@@ -47,19 +54,29 @@ export default function InstrumentRouter(
         return false;
     }
 
-    async function getActiveTodayInstrument(instrument: Instrument) {
+    async function getActiveTodayInstrument(instrument: Instrument): Promise<Instrument | null> {
         const active = await activeToday(instrument);
         log.info(`Active today outputted (${ active }) for instrument (${ instrument.name }) type of (${ typeof active })`);
         if (!active) {
-            return [];
+            return null;
         }
         instrument.surveyTLA = instrument.name.substr(0, 3);
         instrument.link = "https://" + vmExternalWebUrl + "/" + instrument.name + "?LayoutSet=CATI-Interviewer_Large";
         instrument.fieldPeriod = fieldPeriodToText(instrument.name);
-        return [instrument];
+        return instrument;
     }
 
-    // An api endpoint that returns list of installed instruments
+    async function getActiveTodayInstruments(allInstruments: Instrument[]): Promise<Instrument[]> {
+        return (await Promise.all(allInstruments.map(getActiveTodayInstrument)))
+            .filter((result) => result !== null) as Instrument[];
+    }
+
+    async function fetchAllQuestionnaires() {
+        const response: AxiosResponse = await axios.get("http://" + blaiseApiUrl + "/api/v2/cati/questionnaires");
+        const allInstruments: Instrument[] = response.data;
+        return allInstruments;
+    }
+
     instrumentRouter.get("/instruments", async (req: Request, res: Response) => {
         log = req.log;
 
@@ -68,16 +85,10 @@ export default function InstrumentRouter(
         authProvider = new AuthProvider(bimsClientID, log);
 
         try {
-            const response: AxiosResponse = await axios.get("http://" + blaiseApiUrl + "/api/v2/cati/questionnaires");
-            const allInstruments: Instrument[] = response.data;
-            const activeInstruments: Instrument[] = _.flatten(await Promise.all(allInstruments.map(getActiveTodayInstrument)));
-
+            const allInstruments = await fetchAllQuestionnaires();
+            const activeInstruments: Instrument[] = await getActiveTodayInstruments(allInstruments);
             log.info("Retrieved active instruments, " + activeInstruments.length + " item/s");
-
-            const surveys: Survey[] = _.chain(activeInstruments)
-                .groupBy("surveyTLA")
-                .map((value: Instrument[], key: string) => ({ survey: key, instruments: value }))
-                .value();
+            const surveys: Survey[] = groupBySurvey(activeInstruments);
 
             res.json(surveys);
         }
