07c134e3ea0c2a9949e067a3a2586d395c7fde49
jamie kerr <jamiestephenkerr@gmail.com>
2023-10-05 08:49:54 +0100

    fixed health check test

--- server/tests/index.test.ts
+++ server/tests/index.test.ts
@@ -1,8 +1,13 @@
-import app from "../server"; // Link to your server file
 import supertest from "supertest";
+import { IMock, Mock, It } from 'typemoq';
+import BlaiseApiClient from "blaise-api-node-client";
+import nodeServer from "../server";
+
+const blaiseApiMock: IMock<BlaiseApiClient> = Mock.ofType(BlaiseApiClient);
+
+const app = nodeServer(blaiseApiMock.object);
+
 const request = supertest(app);
-
-
 
 describe("Test Heath Endpoint", () => {
     it("should return a 200 status and json message", async () => {
