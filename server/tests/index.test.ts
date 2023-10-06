f9400883ac6d304f1835d5ed99713f48e8e10e89
jamie kerr <jamiestephenkerr@gmail.com>
2023-10-06 14:08:23 +0100

    revert to getting env vars the same as previously

--- server/tests/index.test.ts
+++ server/tests/index.test.ts
@@ -2,19 +2,10 @@ import supertest from "supertest";
 import { IMock, Mock, It } from 'typemoq';
 import BlaiseApiClient from "blaise-api-node-client";
 import nodeServer from "../server";
-import { EnvironmentVariables } from "../Config";
 
 const blaiseApiMock: IMock<BlaiseApiClient> = Mock.ofType(BlaiseApiClient);
-const environmentVariables: EnvironmentVariables = {
-    VM_EXTERNAL_CLIENT_URL: "external-client-url",
-    VM_EXTERNAL_WEB_URL: "external-web-url",
-    BLAISE_API_URL: "mock",
-    CATI_DASHBOARD_URL: "internal-url",
-    BIMS_CLIENT_ID: "mock@id",
-    BIMS_API_URL: "mock-bims-api"
-};
 
-const app = nodeServer(environmentVariables, blaiseApiMock.object);
+const app = nodeServer(blaiseApiMock.object);
 
 const request = supertest(app);
 
