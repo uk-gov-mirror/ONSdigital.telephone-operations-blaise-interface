24c2995cbbdbfc8760947ed68a44e227e0fc62b4
SidraJaved <sidrajaved92@gmail.com>
2023-10-10 10:35:33 +0100

    refactor: typo error and removing try catch block in the unit test for health checkpoint
    Refs: BLAIS5-3811

--- server/tests/index.test.ts
+++ server/tests/index.test.ts
@@ -18,15 +18,11 @@ const app = nodeServer(environmentVariables, blaiseApiMock.object);
 
 const request = supertest(app);
 
-describe("Test Heath Endpoint", () => {
+describe("Test Health Endpoint", () => {
     it("should return a 200 status and json message", async () => {
-        try {
-            const response = await request.get("/tobi-ui/version/health");
+        const response = await request.get("/tobi-ui/version/health");
 
-            expect(response.statusCode).toEqual(200);
-            expect(response.body).toStrictEqual({healthy: true});
-        } catch (error) {
-            console.error(error);
-        }
+        expect(response.statusCode).toEqual(200);
+        expect(response.body).toStrictEqual({ healthy: true });
     });
 });
